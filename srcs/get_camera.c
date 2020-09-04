777edc57d10f7600ba08236c7ea6028f22ab787a
lrcouto <laurarccouto@gmail.com>
2020-09-04 20:06:11 -0300

    Starting camera handling. FIguring out linked lists.

--- srcs/get_camera.c
+++ srcs/get_camera.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_rt.c                                          :+:      :+:    :+:   */
+/*   get_camera.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/18 15:35:24 by lcouto            #+#    #+#             */
+/*   Created: 2020/09/04 19:16:02 by lcouto            #+#    #+#             */
 /*   Updated: 2020/09/04 19:52:16 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minirt.h"
+/*#include "../include/minirt.h"
 
-void	init_rt(t_rt *rt)
+void	get_camera(char *line)
 {
-	t_reso	reso;
-	t_ambi	ambi;
 
-	reso.width = 0;
-	reso.height = 0;
-	reso.qty = 0;
-	ambi.light = 0.0;
-	ambi.color.b = 0;
-	ambi.color.r = 0;
-	ambi.color.g = 0;
-	ambi.qty = 0;
-	rt->reso = reso;
-	rt->ambi = ambi;
-}
+}*/
--- srcs/rt_identify.c
+++ srcs/rt_identify.c
@@ -6,7 +6,7 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 19:08:06 by lcouto            #+#    #+#             */
-/*   Updated: 2020/09/03 18:54:39 by lcouto           ###   ########.fr       */
+/*   Updated: 2020/09/04 19:52:16 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,21 @@
 void	rt_identify(char *line, t_rt *rt)
 {
 	if (line[0] == 'R' && line[1] == ' ')
+	{
+		rt->reso.qty = rt->reso.qty + 1;
+		if (rt->reso.qty > 1)
+			errormsg(9);
 		get_resolution(line, rt);
+	}
 	else if (line[0] == 'A' && line[1] == ' ')
+	{
+		rt->ambi.qty = rt->ambi.qty + 1;
+		if (rt->ambi.qty > 1)
+			errormsg(10);
 		get_ambient(line, rt);
+	}
+	/*else if (line[0] == 'c' && line[1] == ' ')
+		get_camera(line);*/
 	else
 		errormsg(2);
 }
