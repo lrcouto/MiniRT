0506d54eed06384d7739b59d8aed9f4410630bd9
lrcouto <laurarccouto@gmail.com>
2020-09-04 18:34:57 -0300

    Reorganized structs

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 15:57:15 by lcouto            #+#    #+#             */
-/*   Updated: 2020/09/04 16:23:20 by lcouto           ###   ########.fr       */
+/*   Updated: 2020/09/04 18:34:57 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,43 +27,63 @@
 # include <stdio.h>
 
 /*
+** Holds values for X, Y, Z coordinates.
+*/
+
+typedef struct		s_coord
+{
+	double		x;
+	double		y;
+	double		z;
+}					t_coord;
+
+/*
+** Holds values for RGB colors.
+*/
+
+typedef struct		s_color
+{
+	int			r;
+	int			g;
+	int			b;
+}					t_color;
+
+/*
 ** Holds values for window size.
 */
 
-typedef struct	s_reso
+typedef struct		s_reso
 {
 	int			width;
 	int			height;
-}				t_reso;
+}					t_reso;
 
 /*
 ** Holds values for ambient lighting.
 */
 
-typedef struct	s_ambi
+typedef struct		s_ambi
 {
 	double		light;
-	int			red;
-	int			gre;
-	int			blu;
-}				t_ambi;
+	t_color		color;
+}					t_ambi;
 
 /*
 ** Holds values from the .rt file.
 */
 
-typedef struct	s_rt
+typedef struct		s_rt
 {
 	t_reso		reso;
 	t_ambi		ambi;
 
-}				t_rt;
+}					t_rt;
 
 /*
 ** Holds values needed for MiniLibX's functions.
 */
 
-typedef struct s_mlx
+typedef struct		s_mlx
 {
 	void		*mlx;
 	void		*win;
@@ -72,18 +92,28 @@ typedef struct s_mlx
 	int			bpp;
 	int			line_leng;
 	int			endian;
-}				t_mlx;
+}					t_mlx;
 
+/*
+** Holds values for cameras.
+*/
 
+typedef struct		s_cam
+{
+	t_coord			view;
+	t_coord			pos;
+	int				fov;
+	struct s_cam	*next;
+}					t_cam;
 
-int				get_next_line(int fd, char **line);
-void			rt_identify(char *line, t_rt *rt);
-void			get_resolution(char *line, t_rt *rt);
-void			init_rt(t_rt *rt);
-void			rt_window(t_rt *rt);
-void			get_ambient(char *line, t_rt *rt);
-void			errormsg(int errornum);
-void			ambi_rgb_values(int i, int check, char *line, t_rt *rt);
-double			ft_atof(char s[]);
+int					get_next_line(int fd, char **line);
+void				rt_identify(char *line, t_rt *rt);
+void				get_resolution(char *line, t_rt *rt);
+void				init_rt(t_rt *rt);
+void				rt_window(t_rt *rt);
+void				get_ambient(char *line, t_rt *rt);
+void				errormsg(int errornum);
+void				ambi_rgb_values(int i, int check, char *line, t_rt *rt);
+double				ft_atof(char s[]);
 
 #endif
--- srcs/init_rt.c
+++ srcs/init_rt.c
@@ -6,7 +6,7 @@
 /*   By: lcouto <lcouto@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 15:35:24 by lcouto            #+#    #+#             */
-/*   Updated: 2020/08/20 15:58:00 by lcouto           ###   ########.fr       */
+/*   Updated: 2020/09/04 18:34:57 by lcouto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	init_rt(t_rt *rt)
 	reso.width = 0;
 	reso.height = 0;
 	ambi.light = 0.0;
-	ambi.blu = 0;
-	ambi.gre = 0;
-	ambi.red = 0;
+	ambi.color.b = 0;
+	ambi.color.r = 0;
+	ambi.color.g = 0;
 	rt->reso = reso;
 	rt->ambi = ambi;
 }
